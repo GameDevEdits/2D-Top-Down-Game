e12d35989309338b0a4bbbc599a5f97862424282
PatrickPiazza <62964078+RustyMew@users.noreply.github.com>
2024-02-16 16:36:35 -0500

    Control display updated

--- Assets/Scripts/ControlsDisplay.cs
+++ Assets/Scripts/ControlsDisplay.cs
@@ -5,26 +5,26 @@ using TMPro;
 
 public class ControlsDisplay : MonoBehaviour
 {
-    public static bool displayControls = false;
+    public bool displayControls = true;
 	[SerializeField]private TextMeshProUGUI controls;
 	
 	void Update()
 	{
-		//Will make the controls display whenever one of the Shift keys is held down
-		if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+		//Will make the controls appear on screen if left or right Shift is pressed while they aren't visible (visible by default)
+		if((Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift)) && !displayControls)
 		{
 			displayControls = true;
 		}
 		
-		//Will remove the controls display when the shift key is released
-		if(Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+		//Will remove the controls display when one of the the Control keys is pressed while the controls are visible
+		if((Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl)) && displayControls)
 		{
 			displayControls = false;
 		}
 		
 		if(displayControls)
 		{
-			controls.text = "WASD: Move Mouse: Aim LMB: Attack RMB: Block";
+			controls.text = "WASD: Move"+System.Environment.NewLine+"Mouse: Aim"+System.Environment.NewLine+"LMB: Normal Attack"+System.Environment.NewLine+"RMB: Block"+System.Environment.NewLine+"Q: Heavy Attack"+System.Environment.NewLine+"Space: Dodge"+System.Environment.NewLine+"E: Interact"+System.Environment.NewLine+"Control: Remove Controls"+System.Environment.NewLine+"Shift: Display Controls";
 		}
 		else
 		{
