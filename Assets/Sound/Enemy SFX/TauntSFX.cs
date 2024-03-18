607add4afa49fa6cb6127468bdb355271ca288e9
GameDevEdits <spoodaman.khurram2006@gmail.com>
2024-03-18 19:39:55 +0000

    Updates

--- Assets/Sound/Enemy SFX/TauntSFX.cs
+++ Assets/Sound/Enemy SFX/TauntSFX.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class TauntSFX : MonoBehaviour
 {
     public AudioSource audioSource; // Assign your audio source in the Inspector
+    public bool playOnce = false; // Toggle this in the Inspector to play sound only once
+
+    private bool hasPlayed = false; // Flag to keep track of whether the sound has been played
 
     // This method will be called from the animation event
     public void PlayTauntSFX()
@@ -14,7 +17,14 @@ public class TauntSFX : MonoBehaviour
             // Check if the audio source is not already playing
             if (!audioSource.isPlaying)
             {
+                // Check if the sound should only be played once and if it has already been played
+                if (playOnce && hasPlayed)
+                {
+                    return; // If so, do not play the sound again
+                }
+
                 audioSource.Play();
+                hasPlayed = true; // Set the flag to true indicating the sound has been played
             }
         }
     }
